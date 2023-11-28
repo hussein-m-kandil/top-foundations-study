0ae2ce0a4fdec39f7e2b309fddeb774eb8885895
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2023-11-28 18:31:30 +0200

    Odin Calculator: Change the structure of JS script

--- odin-calculator/assets/js/main.js
+++ odin-calculator/assets/js/main.js
@@ -3,12 +3,44 @@ const MID_TONE_GRAY = "#7F7F7F";
 const LIGHT_GRAY = "#EFEFEF";
 const WHITE = "#FFFFFF";
 
-const calculator = {
-  "+": (x, y) => x + y,
-  "-": (x, y) => x - y,
-  "*": (x, y) => x * y,
-  "/": (x, y) => x / y,
-  calculate(opStr) {
+document.querySelectorAll("button").forEach((btn) => {
+  addBtnInteractivityEffects(btn);
+});
+
+function addBtnInteractivityEffects(btn) {
+  const currentBgColor = btn.style.backgroundColor;
+  btn.addEventListener("pointerenter", (event) => {
+    if (event.pointerType === "mouse") {
+      btn.style.backgroundColor = LIGHT_GRAY;
+      if (event.bubbles) event.stopPropagation();
+    }
+  });
+  btn.addEventListener("pointerdown", (event) => {
+    event.preventDefault();
+    btn.style.backgroundColor = LIGHT_GRAY;
+    btn.style.transform = "scale(90%)";
+    if (event.bubbles) event.stopPropagation();
+  });
+  btn.addEventListener("pointerup", (event) => {
+    event.preventDefault();
+    btn.style.transform = "scale(100%)";
+    if (event.bubbles) event.stopPropagation();
+  });
+  btn.addEventListener("pointerleave", (event) => {
+    event.preventDefault();
+    btn.style.backgroundColor = currentBgColor;
+    btn.style.transform = "scale(100%)";
+    if (event.bubbles) event.stopPropagation();
+  });
+}
+
+function Calculator() {
+  this["+"] = (x, y) => x + y;
+  this["-"] = (x, y) => x - y;
+  this["*"] = (x, y) => x * y;
+  this["/"] = (x, y) => x / y;
+
+  this.calculate = (opStr) => {
     const opArr = opStr.split(" ");
     const x = +opArr[0];
     const operator = opArr[1];
@@ -22,42 +54,13 @@ const calculator = {
       }
       return result;
     }
-  },
-};
-// Test calculator
-/* console.log("-2 + 5 = ", calculator.calculate("-2 + 5"));
-console.log("5 - 7 = ", calculator.calculate("5 - 7"));
-console.log("5 * 7 = ", calculator.calculate("5 * 7"));
-console.log("-6 / 3 = ", calculator.calculate("-6 / 3"));
-console.log("3 / 5 = ", calculator.calculate("3 / 5"));
-console.log("3 / 7 = ", calculator.calculate("3 / 7")); */
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Button interactivity effects
-  document.querySelectorAll("button").forEach((btn) => {
-    const currentBgColor = btn.style.backgroundColor;
-    btn.addEventListener("pointerenter", (event) => {
-      if (event.pointerType === "mouse") {
-        btn.style.backgroundColor = LIGHT_GRAY;
-        if (event.bubbles) event.stopPropagation();
-      }
-    });
-    btn.addEventListener("pointerdown", (event) => {
-      event.preventDefault();
-      btn.style.backgroundColor = LIGHT_GRAY;
-      btn.style.transform = "scale(90%)";
-      if (event.bubbles) event.stopPropagation();
-    });
-    btn.addEventListener("pointerup", (event) => {
-      event.preventDefault();
-      btn.style.transform = "scale(100%)";
-      if (event.bubbles) event.stopPropagation();
-    });
-    btn.addEventListener("pointerleave", (event) => {
-      event.preventDefault();
-      btn.style.backgroundColor = currentBgColor;
-      btn.style.transform = "scale(100%)";
-      if (event.bubbles) event.stopPropagation();
-    });
-  });
-});
+  };
+}
+/* // Test calculator
+const testCalc = new Calculator();
+console.log("-2 + 5 = ", testCalc.calculate("-2 + 5"));
+console.log("5 - 7 = ", testCalc.calculate("5 - 7"));
+console.log("5 * 7 = ", testCalc.calculate("5 * 7"));
+console.log("-6 / 3 = ", testCalc.calculate("-6 / 3"));
+console.log("3 / 5 = ", testCalc.calculate("3 / 5"));
+console.log("3 / 7 = ", testCalc.calculate("3 / 7")); */
