b9e62551e70ea860d7e52a881ae52db410844524
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2023-11-30 22:31:15 +0200

    Odin Calculator: Add logic the populates the display

--- odin-calculator/assets/js/main.js
+++ odin-calculator/assets/js/main.js
@@ -3,8 +3,64 @@ const MID_TONE_GRAY = "#7F7F7F";
 const LIGHT_GRAY = "#EFEFEF";
 const WHITE = "#FFFFFF";
 
+// Get calculator's display
+const calcDisplay = document.querySelector("#calc-display");
+
+// Get calculator's buttons
 document.querySelectorAll("button").forEach((btn) => {
+  switch (btn.textContent) {
+    // Add values to buttons for later user
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      btn.value = " " + btn.textContent + " ";
+      break;
+    default:
+      btn.value = btn.textContent;
+      break;
+  }
   addBtnInteractivityEffects(btn);
+  btn.addEventListener("click", (event) => {
+    switch (event.target.value) {
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "0":
+      case ".":
+      case " + ":
+      case " - ":
+      case " * ":
+      case " / ":
+        if (calcDisplay.textContent.length === 0) {
+          calcDisplay.textContent = event.target.value;
+        } else {
+          calcDisplay.textContent += event.target.value;
+        }
+        break;
+      case "CE":
+        if (calcDisplay.textContent.length > 0) {
+          calcDisplay.textContent = calcDisplay.textContent.trimEnd();
+          calcDisplay.textContent = calcDisplay.textContent.slice(
+            0,
+            calcDisplay.textContent.length - 1
+          );
+        }
+        break;
+      case "AC":
+        calcDisplay.textContent = ""; // TODO: free the state of the app
+        break;
+      case "=":
+      // Calculate
+    }
+    if (event.bubbles) event.stopPropagation();
+  });
 });
 
 function addBtnInteractivityEffects(btn) {
